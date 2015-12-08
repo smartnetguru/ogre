12e3a627180814ce8f8f579781601357dc5f3bf0
Derek <saif.tareq@gmail.com>
2015-12-08 04:20:21 -0800

    Use the resources route instead of the custom ones (except delete for now)

--- app/controllers/resume_controller.rb
+++ app/controllers/resume_controller.rb
@@ -2,7 +2,7 @@ class ResumeController < ApplicationController
   def new
     resume = Resume.create user_id: current_user.id
     if resume.valid?
-      redirect_to "/resume/#{resume.id}/edit"
+      redirect_to edit_resume_path(resume)
     else
       redirect_to :root_path
     end
@@ -13,6 +13,10 @@ class ResumeController < ApplicationController
   def delete
     resume = Resume.where(id: params['id']).first
     resume.destroy if not resume.nil?
+    # not sure why
+    # redirect_to :root_path
+    # doesn't work here. I really hope it's not part of a
+    # bigger problem
     redirect_to '/'
   end
 end
