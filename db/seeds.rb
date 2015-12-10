d79d381b0fe5f7e8c9531e99cc5a2f0334af6846
Derek <saif.tareq@gmail.com>
2015-12-10 00:06:56 -0800

    Add some real sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,7 @@ user.password_confirmation = 'asdfasdf'
 user.skip_confirmation!
 user.save!
 
+# Beginning of nonsense for the sake of testing
 job1 = Job.create({
   user_id: user.id,
   company: 'ACME',
@@ -92,4 +93,97 @@ project2 = Project.create({
     resume: resume,
     name: skill
   })
+end
+
+# Beginning of serious fake/real resume for testing export functionalities
+resume = Resume.create({
+  user_id: user.id,
+  name: 'Slack Resume - Frontend Engineer',
+  desc: 'First draft',
+  website: 'www.dereksaif.com',
+  email: 'saif.tareq@gmail.com',
+  contactname: 'Tareq (Derek) Saif',
+  phone: '510-555-5555',
+  address: '123 Fake St. Richmond, CA'
+})
+
+job2 = Job.create({
+  user_id: user.id,
+  company: 'University of California Berkeley',
+  position: 'Information Systems Analyst',
+  location: 'Berkeley, CA',
+  start: '01-05-2014',
+  end: '01-06-2015'
+})
+
+job3 = Job.create({
+  user_id: user.id,
+  company: 'Lawrence Berkeley National Laboratory',
+  position: 'Computer Systems Engineer Trainee',
+  location: 'Berkeley, CA',
+  start: '01-09-2011',
+  end: '01-12-2012'
+})
+
+job1 = Job.create({
+  user_id: user.id,
+  company: 'Tekk Support',
+  position: 'Business Owner / Principal Technician',
+  location: 'San Pablo, CA',
+  start: '01-03-2013',
+  end: '01-05-2014'
+})
+
+# Job3 responsibilities
+[
+  'Managed web precence (Yelp, Facebook, Twitter)',
+  'Created business page www.tekk-support.com on personal server including a contact page \
+  backed by google\'s SMTP library and dynamic based on device',
+  'Brought in clients through grassroots advertising partnerships with local businesses \
+  and a successful flyering campaign'
+].each_with_index do |res, i|
+  resp = Responsibility.create({
+    job_id: job1.id,
+    text: res
+  })
+  resume.responsibilitys.push resp
+end
+
+# Job 2 responsibilities
+[
+  'Rethemed Sir Trevor JS (a library for content management that has no built \
+  in theming functionality',
+  'Extended Sir Trevor JS with custom blocks including arbitrary file upload',
+  'Organized SASS files for one rails app deployed to two places with different themes \
+  with asset precompilation based on environment variables',
+  'Used AWS JavaScript SDK for client side uploading (progress bar + bypassing our server for performance)',
+  'Used Shapeshift and Draggable jQuery libraries to create trough-like user interface',
+  'Created jQuery plugin for autosave dialog functionality (comparable to Gmail\'s saving/saved dialog)',
+  'Created jQuery plugin for creating an interactive vertical calendar (similar to Chronoline.js)',
+  'Deployed Win64 binary to Heroku to run ACER ConQuest analysis software',
+  'Responsibile for frontend work (all CSS and supplementary JS)'
+].each_with_index do |res, i|
+  resp = Responsibility.create({
+    job_id: job2.id,
+    text: res
+  })
+  resume.responsibilitys.push resp
+end
+
+# Job 3 responsibilities
+[
+  'Designed and implemented tools for provisioning and managing virtual machines and routers \
+  to simulate specific network conditions',
+  'Created a web frontend for these tools including user authentication that allowed users to \
+  reserve computing time with a specifically configured network',
+  'Implemented system monitoring to ensure availability of server (physical state through IPMI, \
+  ping availability, and shell access)',
+  'Automated creation of virtual machines including user and software configuration with a \
+  single BASH command (for use by admins)'
+].each_with_index do |res, i|
+  resp = Responsibility.create({
+    job_id: job3.id,
+    text: res
+  })
+  resume.responsibilitys.push resp
 end
