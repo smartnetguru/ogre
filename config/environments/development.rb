ca25001b692bd4929c9aed0049bb9d613f182345
Derek <saif.tareq@gmail.com>
2015-12-25 00:50:06 -0500

    Added notes for setting up and domain name for mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   # is an example of default_url_options appropriate for a development environment
   # in config/environments/development.rb:
   # In production, :host should be set to the actual host of your application.
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'resgen.dereksaif.com', port: 80 }
 
   config.action_mailer.smtp_settings = {
     address: ENV['mailer_address'],
