e3bef8949f5c12b5f6afe724366f30db85c0c205
Derek <saif.tareq@gmail.com>
2015-12-27 17:27:21 -0800

    Be more descriptive of whats missing

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -11,7 +11,7 @@ class Resume < ActiveRecord::Base
     return name
   end
   def desc_text
-    return I18n.t('blank') if desc.nil? or desc.empty?
+    return I18n.t('no_description') if desc.nil? or desc.empty?
     return desc
   end
   def get_relevant_jobs_and_responsibilities
